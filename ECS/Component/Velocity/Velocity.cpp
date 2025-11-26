19efda3f48e2f3115a921e448aa8de439dd390f4
Pierre Ryssen <pierre.ryssen@epitech.eu>
2025-11-26 19:50:56 +0100

    docs (Velocity): add the documentation of the velocity component

--- ECS/Component/Velocity/Velocity.cpp
+++ ECS/Component/Velocity/Velocity.cpp
@@ -7,18 +7,30 @@
 
 #include "Velocity.hpp"
 
+/**
+ * @brief Constructs a new Velocity component with specified velocity.
+ * @param velocity The velocity value.
+*/
 Velocity::Velocity(float velocity) : _velocity(velocity)
 {
 }
 
+/**
+ * @brief Gets the velocity of an entity.
+ *
+ * @return A float to the actual velocity of the entity.
+ */
 float Velocity::getVelocity() const
 {
     return _velocity;
 }
 
+/**
+ * @brief Updates the velocity of an entity.
+ *
+ * @param velocity value used to change the velocity of the entity.
+ */
 void Velocity::setVelocity(float velocity)
 {
     _velocity = velocity;
 }
-
-
