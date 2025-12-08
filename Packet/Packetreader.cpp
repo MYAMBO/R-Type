1d5d945b122f6ee5e462674acd9067c355b23090
timote1koenig <timote.koenig@epitech.eu>
2025-12-08 22:36:36 +0100

    feat: interpret and time stamp functions

--- Packet/Packetreader.cpp
+++ Packet/Packetreader.cpp
@@ -6,3 +6,31 @@
 */
 
 #include "Packetreader.hpp"
+
+#include <functional>
+#include <unordered_map>
+#include <utility>
+
+Packetreader::Packetreader(std::string data, const bool isClient) : _isClient(isClient), _data(std::move(data)){}
+
+void Packetreader::interpretPacket()
+{
+    int code = std::stoi( _data.substr(0, 2), nullptr, 16);
+
+    switch (code) {
+        case 0x06: timestamp(); break;
+        case 0x07: updateEntity(); break;
+        case 0x08: hit(); break;
+        case 0x09: dead(); break;
+        case 0x0A: endGame(); break;
+        default:
+            break;
+    }
+}
+
+void Packetreader::timestamp() const
+{
+    int  time = std::stoi(_data.substr(0, 4), nullptr, 16);
+
+    //call function and give int
+}
--- Packet/Packetreader.hpp
+++ Packet/Packetreader.hpp
@@ -7,10 +7,24 @@
 
 #ifndef R_TYPE_PACKETREADER_HPP
 #define R_TYPE_PACKETREADER_HPP
-
+#include <string>
 
 class Packetreader
 {
+    public:
+        Packetreader(std::string data, bool isClient = false);
+        ~Packetreader() = default;
+
+        void interpretPacket();
+    private:
+        bool _isClient;
+        std::string _data;
+
+        void timestamp() const;
+        void updateEntity();
+        void hit();
+        void dead();
+        void endGame();
 };
 
 
