e70ae2e56128d91dd1663133d443dc890035c753
timote1koenig <timote.koenig@epitech.eu>
2025-12-08 14:55:42 +0100

    feat: empty Packetreader class

---
+++ Packet/Packetreader.cpp
@@ -0,0 +1,8 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Packetreader.cpp
+*/
+
+#include "Packetreader.hpp"
---
+++ Packet/Packetreader.hpp
@@ -0,0 +1,17 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Packetreader.hpp
+*/
+
+#ifndef R_TYPE_PACKETREADER_HPP
+#define R_TYPE_PACKETREADER_HPP
+
+
+class Packetreader
+{
+};
+
+
+#endif //R_TYPE_PACKETREADER_HPP
