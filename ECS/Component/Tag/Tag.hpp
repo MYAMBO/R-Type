b62777eb45c7fd60c36668177c5232e876d2a3da
Pierre Ryssen <pierre.ryssen@epitech.eu>
2025-12-08 14:17:15 +0100

    feat (Tag): add the tag componenent

---
+++ ECS/Component/Tag/Tag.hpp
@@ -0,0 +1,36 @@
+/*
+** EPITECH PROJECT, 2025
+** R-Type
+** File description:
+** Tag
+*/
+
+
+#ifndef TAG_HPP_
+    #define TAG_HPP_
+
+#include <iostream>
+
+#include "Component.hpp"
+
+/**
+ * @brief Tag component to give a name on an entity and find a specific entity easier.
+ *
+ * This component encapsulates a tag name.
+ * It provides methods to retrieve and modify the tag name.
+*/
+class Tag : public Component {
+
+    public:
+        Tag(std::string &tag);
+        virtual ~Tag() = default;
+
+    [[nodiscard]] std::string getTag() const;
+
+    void setTag(const std::string &tag);
+
+    private:
+        std::string _tag;
+};
+
+#endif //TAG_HPP_
