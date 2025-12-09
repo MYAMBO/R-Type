cad467a54a60f76bd2462581fddef2cb7fa8cfb2
MartinB-E <martin.bonte@epitech.eu>
2025-12-09 11:09:57 +0100

    feat(Updater): add camera management in updater

--- ECS/System/Updater/Updater.cpp
+++ ECS/System/Updater/Updater.cpp
@@ -10,6 +10,7 @@
 #include "Scale.hpp"
 #include "Sprite.hpp"
 #include "Sprite.hpp"
+#include "Camera.hpp"
 #include "Updater.hpp"
 #include "Velocity.hpp"
 #include "Position.hpp"
@@ -31,6 +32,7 @@ Updater::Updater()
 */
 void Updater::update(const float& dt, World &w)
 {
+    updateCameras(dt, w);
     updateAnimations(dt, w);
     updateSprites(dt, w);
 }
@@ -87,5 +89,23 @@ void Updater::updateAnimations(const float &dt, World &w)
             anim->setCurrentFrame(anim->getCurrentFrame() + 1);
         }
         sprite->setTextureRect(anim->getFrameRect());
+    }
+}
+
+/**
+* @brief Update the camera of all entities if they have it in the world
+* @param dt Delta time since last update
+* @param w Reference to the world containing entities and components
+*/
+void Updater::updateCameras(const float &dt, World &w)
+{
+    for (auto &entity : w.getAllEntitiesWithComponent<Camera>()) {
+        auto cameraComp = entity->getComponent<Camera>();
+        if (!cameraComp)
+            continue; 
+        auto posComp = entity->getComponent<Position>();
+        if (posComp)
+            cameraComp->setPosition({posComp->getX(), posComp->getY()});
+        w.getWindow()->setView(cameraComp->getView());
     }
 }
--- ECS/System/Updater/Updater.hpp
+++ ECS/System/Updater/Updater.hpp
@@ -5,9 +5,10 @@
 ** Updater
 */
 
-#pragma once
+#ifndef UPDATER_HPP_
+    #define UPDATER_HPP_
 
-#include "World.hpp"
+    #include "World.hpp"
 
 /**
  * @brief Updater system responsible for updating various components of entities.
@@ -23,6 +24,9 @@ class Updater : public System {
         void update(const float& dt, World &w) override;
     private:
         void updateSprites(const float& dt, World &w);
-        void updateBoxColliders(const float& dt, World &w);
+        void updateCameras(const float& dt, World &w);
         void updateAnimations(const float& dt, World &w);
+        void updateBoxColliders(const float& dt, World &w);
 };
+
+#endif /* !UPDATER_HPP_ */
