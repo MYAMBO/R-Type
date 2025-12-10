7f783ace185730a1a2e6c667a001fa72871903ab
Antoiix <antoine.orange@epitech.eu>
2025-12-10 10:25:43 +0100

    feat(Server): server parser

--- Server/Server.cpp
+++ Server/Server.cpp
@@ -2,7 +2,7 @@
 ** EPITECH PROJECT, 2025
 ** R-Type
 ** File description:
-** server class declarations
+** Server.cpp
 */
 
 #include "Server.hpp"
@@ -16,8 +16,55 @@ Server::Server()
 
 auto Server::parse(int ac, char **av) -> void
 {
-    sf::TcpListener listener;
-    if (listener.listen(port) != sf::Socket::Status::Done)
+    char *endPtr;
+
+    for (int opt = getopt(ac, av, "p:u:hd"); opt != -1; opt = getopt(ac, av, "p:u:hd"))
+        switch(opt) {
+            case 'p':
+                _tcpPort = strtol(optarg, &endPtr, 10);
+                if (endPtr == optarg)
+                    _tcpPort = -1;
+                continue;
+
+            case 'u':
+                _udpPort = strtol(optarg, &endPtr, 10);
+                if (endPtr == optarg)
+                    _udpPort = -1;
+                continue;
+
+            case 'd':
+                _debugMode = true;
+                break;
+
+            case 'h':
+                std::cout << "R-TYPE Server - USAGE" << std::endl;
+                std::cout << "\t./r-type_server [-d] [-p TcpPort] [-u UdpPort]" << std::endl;
+                std::cout << std::endl << "\td : debug mode" << std::endl;
+                std::cout <<  "\tp : specify the TCP port (mandatory)" << std::endl;
+                std::cout <<  "\tu : specify the UDP port (mandatory)" << std::endl;
+                break;
+
+            default :
+                std::cerr << "R-TYPE Server - USAGE" << std::endl;
+                std::cerr << "\t./r-type_server [-d] [-p TcpPort] [-u UdpPort]" << std::endl;
+                std::cerr << std::endl << "\td : debug mode" << std::endl;
+                std::cerr <<  "\tp : specify the TCP port (mandatory)" << std::endl;
+                std::cerr <<  "\tu : specify the UDP port (mandatory)" << std::endl;
+                throw InitServerException("");
+        }
+    if (_tcpPort == -1)
+        throw InitServerException("TCP port must be specified. Check the helper with the -h option.");
+    if (_udpPort == -1)
+        throw InitServerException("UDP port must be specified. Check the helper with the -h option.");
+}
+
+
+auto Server::initServer() -> void
+{
+    _tcpListener = sf::TcpListener();
+    _udpSocket = sf::UdpSocket();
+
+    if (_tcpListener.listen(_tcpPort) != sf::Socket::Status::Done)
         throw InitServerException();
 
     sf::TcpSocket client;
