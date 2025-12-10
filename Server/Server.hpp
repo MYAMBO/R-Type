28717c3b52f57253716d9288a499c6605ffb9577
timote1koenig <timote.koenig@epitech.eu>
2025-12-10 11:27:20 +0100

    fix: remove pragmaOnce

--- Server/Server.hpp
+++ Server/Server.hpp
@@ -5,7 +5,8 @@
 ** Server.hpp
 */
 
-#pragma once
+#ifndef SEVER
+#define SERVER
 
 #include <thread>
 #include <utility>
@@ -18,39 +19,42 @@
 
 class Server
 {
-public:
-    Server();
-    ~Server() = default;
-    void initServer();
-    void start();
-    void parse(int ac, char **av);
-    void log(const std::string& message) const;
-    void udpThread();
-    void tcpThread();
-
-    class InitServerException : public std::exception
-    {
     public:
-        explicit InitServerException(std::string message = "")
-        {
-            _message = std::move(message);
-        };
-        [[nodiscard]] const char* what() const noexcept override
-        {
-            return _message.c_str();
-        }
-    private:
-        std::string _message;
-    };
+        Server();
+        ~Server() = default;
+        void initServer();
+        void start();
+        void parse(int ac, char** av);
+        void log(const std::string& message) const;
+        void udpThread();
+        void tcpThread();
 
-private:
-    sf::UdpSocket _udpSocket;
-    sf::TcpListener _tcpListener;
-    sf::TcpSocket _tcpClient;
-    std::thread _udpThread;
-    long _tcpPort;
-    long _udpPort;
-    bool _debugMode;
-    std::vector<User> _users;
-    std::mutex _mutex;
+        class InitServerException : public std::exception
+        {
+            public:
+                explicit InitServerException(std::string message = "")
+                {
+                    _message = std::move(message);
+                };
+
+                [[nodiscard]] const char* what() const noexcept override
+                {
+                    return _message.c_str();
+                }
+
+            private:
+                std::string _message;
+        };
+
+    private:
+        sf::UdpSocket _udpSocket;
+        sf::TcpListener _tcpListener;
+        sf::TcpSocket _tcpClient;
+        std::thread _udpThread;
+        long _tcpPort;
+        long _udpPort;
+        bool _debugMode;
+        std::vector<User> _users;
+        std::mutex _mutex;
 };
+#endif // SERVER
--- Server/User.hpp
+++ Server/User.hpp
@@ -5,25 +5,29 @@
 ** User.hpp
 */
 
-#pragma once
+#ifndef USER
+#define USER
+
 #include <memory>
 #include "SFML/Network.hpp"
 
 class User
 {
-public:
-    User(unsigned int port, std::string ip, std::shared_ptr<sf::TcpSocket> socket);
-    ~User() = default;
-    std::shared_ptr<sf::TcpSocket> getSocket() const;
-    unsigned int getId() const;
-    unsigned int getPort() const;
-    std::string getName() const;
-    std::string getIp() const;
+    public:
+        User(unsigned int port, std::string ip,
+             std::shared_ptr<sf::TcpSocket> socket);
+        ~User() = default;
+        std::shared_ptr<sf::TcpSocket> getSocket() const;
+        unsigned int getId() const;
+        unsigned int getPort() const;
+        std::string getName() const;
+        std::string getIp() const;
 
-private:
-    std::shared_ptr<sf::TcpSocket> _socket;
-    unsigned int _id;
-    unsigned int _port;
-    std::string _name;
-    std::string _ip;
+    private:
+        std::shared_ptr<sf::TcpSocket> _socket;
+        unsigned int _id;
+        unsigned int _port;
+        std::string _name;
+        std::string _ip;
 };
+#endif // USER
