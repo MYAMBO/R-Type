8cdab31e93f2b200741a96b30b68d5f3886b1322
MartinB-E <martin.bonte@epitech.eu>
2025-12-30 18:46:30 +0100

    feat(Game): add handling of error when not a component and fix crash midgame

--- ECS/Component/Layer/Layer.cpp
+++ ECS/Component/Layer/Layer.cpp
@@ -11,15 +11,24 @@
  * @brief Constructs a new Layer with the layer id.
  * @param id Is the layer number
 */
-Layer::Layer(const int id) : _id(id) {}
+Layer::Layer(const int id)
+    : _id(id)
+{
+}
 
 /**
  * @brief Gets the layer id.
 */
-int Layer::getLayerId() const { return _id; }
+int Layer::getLayerId() const
+{
+    return _id;
+}
 
 
 /**
  * @brief Sets the layer id.
 */
-void Layer::setLayerId(const int id) { _id = id; }
+void Layer::setLayerId(const int id)
+{
+    _id = id;
+}
--- ECS/System/Movement/Movement.cpp
+++ ECS/System/Movement/Movement.cpp
@@ -20,10 +20,12 @@ void Movement::update(const float& dt, World &w)
     for (const auto &entity : w.getAllEntitiesWithComponent<Position>()) {
         const auto posComp = entity->getComponent<Position>();
         const auto velComp = entity->getComponent<Velocity>();
+        if (!posComp)
+            continue;
         if (posComp && velComp) {
             posComp->setX(posComp->getX() + velComp->getVelocityX() * dt);
             posComp->setY(posComp->getY() + velComp->getVelocityY() * dt);
-        } else if (posComp) {
+        } else if (posComp && !velComp) {
             posComp->setX(posComp->getX());
             posComp->setY(posComp->getY());
         }
