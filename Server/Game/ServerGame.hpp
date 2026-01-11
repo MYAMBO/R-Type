6ba2b9480ffe57383612c1a91d8909ccbb10c62f
Pierre Ryssen <pierre.ryssen@epitech.eu>
2026-01-11 13:05:40 +0100

    feat (ServerGame): handle creation and movement of the enemies

--- Server/Game/ServerGame.hpp
+++ Server/Game/ServerGame.hpp
@@ -10,18 +10,19 @@
 
 #include <SFML/System.hpp>
 #include "IGameNetwork.hpp"
-    #include "World.hpp"
+#include "World.hpp"
+#include "EntitiesType.hpp"
 
     #define NB_PLAYER_TO_START 2
     #define MAX_PLAYER 4
 
-enum entitiesType
-{
-    None = 0,
-    Player = 1,
-    Enemy = 2,
-    Bullet = 3
-};
+// enum entitiesType
+// {
+//     None = 0,
+//     Player = 1,
+//     Enemy = 2,
+//     Bullet = 3
+// };
 
 class ServerGame {
     public:
@@ -33,14 +34,20 @@ class ServerGame {
         void handleShoot(int id);
         void handleNewPlayer();
         void startLevel(int levelId);
+        void createEnemy(float x, float y);
+        void createSinusEnemy(float x, float y);
+        // void createShootingEnemy(float x, float y);
+        // void createSinusShootingEnemy(const float x, const float y);
 
     private:
         void createPlayer(float x, float y);
-        void createEnemy(float x, float y);
         void createWave();
         void createBullet(float x, float y);
         void EnemyMovement(int entityId, World &world);
+        void EnemySinusMovement(int entityId, World &world);
         void BulletMovement(int entityId, World &world);
+        void checkDeaths();
+
 
         World _world;
         IGameNetwork& _network;
