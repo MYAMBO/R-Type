ed25f9f29f5d52766e16d7c90d10fc7c41540be4
MartinB-E <martin.bonte@epitech.eu>
2026-01-13 11:22:54 +0100

    ref(GAME): refractor creator

---
+++ Client/Factory/Factory.cpp
@@ -0,0 +1,79 @@
+/*
+** EPITECH PROJECT, 2026
+** R-Type
+** File description:
+** Factory
+*/
+
+#include "Factory.hpp"
+#include "Game.hpp"
+
+Factory::Factory(World& world)  :
+    _uiFactory(world), _worldFactory(world),
+    _effectFactory(world), _toolsFactory(world)
+{
+}
+
+void Factory::createKayu()
+{
+    _effectFactory.createKayu();
+}
+
+void Factory::createMyambo()
+{
+    _effectFactory.createMyambo();
+}
+
+void Factory::createCamera()
+{
+    _worldFactory.createCamera();
+}
+
+void Factory::createOptionsMenu()
+{
+    _uiFactory.createOptionsMenu();
+}
+
+void Factory::createCredits()
+{
+    _effectFactory.createCredits();
+}
+
+void Factory::createMenu()
+{
+    _uiFactory.createMenu();
+}
+void Factory::createGameTools()
+{
+    _toolsFactory.createGameTools();
+}
+
+void Factory::createLoadingScreen()
+{
+    _uiFactory.createLoadingScreen();
+}
+
+void Factory::createPlayer(uint64_t id)
+{
+    _worldFactory.createPlayer(static_cast<int>(id));
+}
+
+void Factory::createBackground(sf::RenderWindow& window)
+{
+    _worldFactory.createBackground(window);
+}
+
+void Factory::createEnemy(float x, float y, int type, int entityId)
+{
+    _worldFactory.createEnemy(x, y, type, entityId);
+}
+
+void Factory::createBullet(size_t entityId, int x, int y, int type)
+{
+    _worldFactory.createBullet(entityId, x, y, type);
+}
+
+void Factory::createSparks(World &world, float x, float y, int amount)
+{
+    _effectFactory.createSparks(world, x, y, amount);
+}
