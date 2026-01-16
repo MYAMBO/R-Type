48de27f1808297595811c28abfea09fee356ce65
MartinB-E <martin.bonte@epitech.eu>
2026-01-16 20:12:09 +0100

    feat(Game): add moaning to the enemy

--- Client/GameHelper/Graphical/GameHelperGraphical.cpp
+++ Client/GameHelper/Graphical/GameHelperGraphical.cpp
@@ -64,10 +64,12 @@ void GameHelperGraphical::createBasicEnemy(World &world, float x, float y, int e
     enemy->addComponent<Data>(std::map<std::string, std::string>{
         {"score", "+100"},
         {"type", "basic"},
-        {"death_sound", "../assets/sounds/killed_first.mp3"}
+        {"death_sound", "../assets/sounds/killed_first.mp3"},
+        {"sound_cooldown", "1"}
     });
     enemy->addComponent<Tag>("enemy");
     enemy->addComponent<BoxCollider>(66.0f, 60.0f);
+    enemy->addComponent<Script>(enemyScript);
 }
 
 void GameHelperGraphical::createFastEnemy(World &world, float x, float y, int entityId)
@@ -83,10 +85,12 @@ void GameHelperGraphical::createFastEnemy(World &world, float x, float y, int en
     enemy->addComponent<Data>(std::map<std::string, std::string>{
         {"score", "+150"},
         {"type", "fast"},
-        {"death_sound", "../assets/sounds/killed_second.mp3"}
+        {"death_sound", "../assets/sounds/killed_second.mp3"},
+        {"sound_cooldown", "1"}
     });
     enemy->addComponent<Tag>("enemy");
     enemy->addComponent<BoxCollider>(66.0f, 60.0f);
+    enemy->addComponent<Script>(enemyScript);
 }
 
 void GameHelperGraphical::createTankEnemy(World &world, float x, float y, int entityId)
@@ -102,10 +106,12 @@ void GameHelperGraphical::createTankEnemy(World &world, float x, float y, int en
     enemy->addComponent<Data>(std::map<std::string, std::string>{
         {"score", "+200"},
         {"type", "tank"},
-        {"death_sound", "../assets/sounds/killed_first.mp3"}
+        {"death_sound", "../assets/sounds/killed_first.mp3"},
+        {"sound_cooldown", "100"}
     });
     enemy->addComponent<Tag>("enemy");
     enemy->addComponent<BoxCollider>(66.0f, 60.0f);
+    enemy->addComponent<Script>(enemyScript);
 }
 
 /**
@@ -128,10 +134,12 @@ void GameHelperGraphical::createSinusEnemy(World &world, float x, float y, int e
     enemy->addComponent<Data>(std::map<std::string, std::string>{
         {"score", "+300"},
         {"type", "sinus"},
-        {"death_sound", "../assets/sounds/killed_second.mp3"}
+        {"death_sound", "../assets/sounds/killed_second.mp3"},
+        {"sound_cooldown", "300"}
     });
     enemy->addComponent<Tag>("enemy");
     enemy->addComponent<BoxCollider>(66.0f, 60.0f);
+    enemy->addComponent<Script>(enemyScript);
 }
 
 /**
@@ -154,10 +162,12 @@ void GameHelperGraphical::createShootingEnemy(World &world, float x, float y, in
     enemy->addComponent<Data>(std::map<std::string, std::string>{
         {"score", "+250"},
         {"type", "shooting"},
-        {"death_sound", "../assets/sounds/killed_second.mp3"}
+        {"death_sound", "../assets/sounds/killed_second.mp3"},
+        {"sound_cooldown", "300"}
     });
     enemy->addComponent<Tag>("enemy");
     enemy->addComponent<BoxCollider>(66.0f, 60.0f);
+    enemy->addComponent<Script>(enemyScript);
 }
 
 /**
@@ -228,7 +238,7 @@ void GameHelperGraphical::soundEffectEntity(const std::string& filepath, float v
     auto soundEntity = world.createEntity();
     soundEntity->addComponent<SoundEffect>(filepath, volume);
     soundEntity->addComponent<Scene>(scene);
-    soundEntity->addComponent<Tag>("sound_effect");
+    soundEntity->addComponent<Tag>("sound_effect" + std::to_string(soundEntity->getId()));
     soundEntity->getComponent<SoundEffect>()->play();
     soundEntity->addComponent<Script>([](int id, World& w) {
         auto entity = GameHelper::getEntityById(w, id);
@@ -330,6 +340,34 @@ void GameHelperGraphical::createScoreGUI(World &world, float x, float y, const s
             data->setData("lifetime", std::to_string(life - 1));
             data->setData("hue", std::to_string(hue));
         } else {
+            w.killEntity(id);
+        }
+    });
+}
+
+void GameHelperGraphical::playRandomAmbianceEnemy(World &world)
+{
+    static std::vector<std::string> sounds = {
+        "../assets/sounds/first_moaning.mp3",
+        "../assets/sounds/second_moaning.mp3",
+        "../assets/sounds/third_moaning.mp3"
+    };
+
+    printf("Playing enemy ambiance sound\n");
+    int choice = rand() % sounds.size();
+    
+    auto soundEnt = world.createEntity();
+    soundEnt->addComponent<Scene>(static_cast<int>(SceneType::GAMEPLAY));
+    soundEnt->addComponent<SoundEffect>(sounds[choice], 40.f);
+    float randomPitch = 0.8f + static_cast<float>(rand()) / (static_cast<float>(RAND_MAX / (1.2f - 0.8f)));
+    auto sfx = soundEnt->getComponent<SoundEffect>();
+    sfx->setPitch(randomPitch);
+    if (sfx)
+        sfx->play();
+
+    soundEnt->addComponent<Script>([](int id, World& w) {
+        auto e = GameHelper::getEntityById(w, id);
+        if (e && e->getComponent<SoundEffect>()->getSfStatus() == sf::Sound::Status::Stopped) {
             w.killEntity(id);
         }
     });
