8cd8741143ae490b05135a08461a238fd72f7850
MartinB-E <martin.bonte@epitech.eu>
2026-01-18 15:17:04 +0100

    fix(Game): fix some issue with thread

--- Client/Game/Game.hpp
+++ Client/Game/Game.hpp
@@ -47,14 +47,6 @@ enum PlayerColor {
     GREEN
 };
 
-// enum entitiesType
-// {
-//     None = 0,
-//     Player = 1,
-//     Enemy,
-//     Bullet
-// };
-
 /**
  * @brief Main Game class to handle the game window and loop.
  */
